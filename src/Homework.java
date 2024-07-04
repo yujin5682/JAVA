bdae1f24ab497e492893906b5d6482f9fa6a57bd
yujin5682 <111857536+yujin5682@users.noreply.github.com>
2024-07-04 19:09:01 +0900

    Add files via upload
    과제5

---
+++ src/Homework.java
@@ -0,0 +1,76 @@
+import java.util.ArrayList;
+
+public class Homework {
+
+	public static void main(String[] args) {
+
+		ArrayList<Customer> customers = new ArrayList<Customer>();
+		ArrayList<Product> products = new ArrayList<Product>();
+
+		// 진짜 매장 만들기
+		Store store1 = new Store();
+		store1.name = "매장1";
+
+
+		Customer customer1 = new Customer();
+		customer1.name = "고객1";
+		customer1.age = 10;
+		customer1.phone = "010-1111-1111";
+		customers.add(customer1);
+
+		Customer customer2 = new Customer();
+		customer2.name = "고객2";
+		customer2.age = 20;
+		customer2.phone = "010-2222-2222";
+		customers.add(customer2);
+
+		Customer customer3 = new Customer();
+		customer3.name = "고객3";
+		customer3.age = 30;
+		customer3.phone = "010-3333-3333";
+		customers.add(customer3);
+		
+		store1.setCustomers(customers);
+
+		Product product1 = new Product();
+		product1.name = "물건1";
+		product1.price = 1000;
+		products.add(product1);
+
+		Product product2 = new Product();
+		product2.name = "물건2";
+		product2.price = 2000;
+		products.add(product2);
+
+		Product product3 = new Product();
+		product3.name = "물건3";
+		product3.price = 3000;
+		products.add(product3);
+		
+		store1.setProducts(products);
+
+		System.out.println("고객 수 : " + customers.size() + "명" + " || 제품 갯수 : " + products.size() + "개" + "\n");
+
+		// 1번 고객이 2번 물건을 샀을때, 고객이 산 제품, 고객이 산 총 금액, 제품 전체 금액
+		store1.pay(customer1, product2);
+		store1.pay(customer1, product1);
+
+		store1.pay(customer2, product1);
+
+		// 1번 고객 - 산 제품= 물건2,2000원 총 금액 : 2000
+		// 1번 고객 - 산 제품= 물건1:1000원,물건2:2000원 총 금액 : 3000
+		for (Customer c : store1.getCustomers()) {
+			
+			System.out.println("고객 이름 : " + c.name + "|| 나이 : " + c.age + " || 번호 : " + c.phone + "이 구매한 제품");
+			for (Product bp : c.getBuyProducts()) {
+				System.out.println("제품 이름 : " + bp.name + " || 가격 : " + bp.price);
+			}
+			
+			System.out.println();
+		}
+
+//		System.out.println(customer1.getBuyProducts());
+
+	}
+
+}
---
+++ src/Store.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Store {
+
+	String name;
+	
+	ArrayList<Customer> customers = new ArrayList<>();
+	
+	ArrayList<Product> products = new ArrayList<>();
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public ArrayList<Customer>  getCustomers() {
+		return customers;
+	}
+	public void setCustomers(ArrayList<Customer> customers) {
+		this.customers = customers;
+	}
+	
+	
+	public ArrayList<Product> getProducts(){
+		return products;
+	}
+	public void setProducts(ArrayList<Product> products) {
+		this.products = products;
+	}
+	
+	
+	void pay(Customer customer, Product product) {
+		customer.getBuyProducts().add(product);
+		customer.buyPrice += product.price;
+		product.totalPrice += product.price; 
+		
+		
+		
+	}
+	
+}
